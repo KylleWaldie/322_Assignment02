a32c494a38b04bc2b7ccae75109a438d06e7208e
Kylle Waldie <kwaldie@mtech.edu>
2025-09-22 10:49:21 -0600

    Added factorial function and changed Sin function

--- include/math_ops.c
+++ include/math_ops.c
@@ -28,19 +28,26 @@ float pow(float base, int exponent )
     return res;
 }
 
+float factorial(int n)
+{
+    float result = 1.0;
+    for(int i =2; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
 float sin(int x, int steps)
 {
-    float sum = 0.0;
-    float term = x;
-    int sign = 1;
+    float result = 0.0;
 
-    for(int n = 0; n < steps; n++){
-        if (n > 0) {
-            term = term * (-1) * x * x / ((2 * n) * (2 * n + 1));
-        }
-        sum += term;
+    for (int n = 0; n < steps; ++n) {
+        int exponent = 2 * n + 1;
+        double term = (pow(-1, n) * pow(x, exponent)) / factorial(exponent);
+        result += term;
     }
-    return sum;
+    return result;
 
 }
 
