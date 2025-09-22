74e64448d8d2bf8d2d85b2bda4bbc5ac6bd6205d
Kylle Waldie <kwaldie@mtech.edu>
2025-09-22 10:42:28 -0600

    Added Sin() function to math_ops.c

--- include/math_ops.c
+++ include/math_ops.c
@@ -30,7 +30,17 @@ float pow(float base, int exponent )
 
 float sin(int x, int steps)
 {
-    //Impliment soon
+    float sum = 0.0;
+    float term = x;
+    int sign = 1;
+
+    for(int n = 0; n < steps; n++){
+        if (n > 0) {
+            term = term * (-1) * x * x / ((2 * n) * (2 * n + 1));
+        }
+        sum += term;
+    }
+    return sum;
 
 }
 
