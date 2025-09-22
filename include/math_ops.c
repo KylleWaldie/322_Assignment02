8ed33efcf4d86eb222ea295e319c748f5f79cc16
Kylle Waldie <kwaldie@mtech.edu>
2025-09-22 10:30:34 -0600

    Made changes to result.txt / added path for math ops

---
+++ include/math_ops.c
@@ -0,0 +1,40 @@
+float multiply(float x, float y) 
+{
+    return x * y;
+}
+
+float add(float x, float y) 
+{
+    return x + y;
+}
+
+float square(float x) 
+{
+    return x * x;
+}
+
+float cube(float x) 
+{
+    return x * x * x;
+}
+
+float pow(float base, int exponent ) 
+{
+    float res = 1;
+    for(int i = 0; i < exponent; i++) 
+    {
+        res *= base;
+    }
+    return res;
+}
+
+float sin(int x, int steps)
+{
+    //Impliment soon
+
+}
+
+float cos(int x, int steps)
+{
+    //Impliment soon
+}
