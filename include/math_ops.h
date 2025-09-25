c293ebf292c37d08ff91fb1921f481fff57cf2df
Kylle Waldie <wldkylle@gmail.com>
2025-09-25 00:34:25 -0600

    Added a header file for math_ops.c

---
+++ include/math_ops.h
@@ -0,0 +1,14 @@
+// include/math_ops.h
+#ifndef MATH_OPS_H
+#define MATH_OPS_H
+
+float multiply(float x, float y);
+float add(float x, float y);
+float square(float x);
+float cube(float x);
+float pow(float base, int exponent);
+float factorial(int n);
+float sin(int x, int steps);
+float cos(int x, int steps);
+
+#endif
